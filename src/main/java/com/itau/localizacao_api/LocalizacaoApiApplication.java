7577e561c65935a8841487605dac45ae9d9c10fb
anapsilvestrinf <anapsilvestrinf@gmail.com>
2025-03-20 17:32:12 -0300

    feat: primeiro commit

---
+++ src/main/java/com/itau/localizacao_api/LocalizacaoApiApplication.java
@@ -0,0 +1,13 @@
+package com.itau.localizacao_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalizacaoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalizacaoApiApplication.class, args);
+	}
+
+}
